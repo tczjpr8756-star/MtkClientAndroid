089ebed68bc84fdd38296e1445a43be829a69a68
tczjpr8756-star <tczjpr8756@privaterelay.appleid.com>
2026-09-19 10:42:50 +0000

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.mtkclientandroid"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.mtkclientandroid"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 3
         versionName = "1.1"
 
